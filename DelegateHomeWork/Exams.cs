b854f3854297c90c7a5a9d7e6718a1556fa3a104
Əziz Rəhimov <72435144+ezizrehimov@users.noreply.github.com>
2023-09-28 11:35:53 +0400

    Homework completed

---
+++ DelegateHomeWork/Exams.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DelegateHomeWork
+{
+    internal class Exams
+    {
+
+        public string Student { get; set; }
+        public int Point { get; set; }
+        public string Subject { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Exams(string student, int point, string subject, DateTime startDate, DateTime endDate)
+        {
+            Student = student;
+            Point = point;
+            Subject = subject;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
---
+++ DelegateHomeWork/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Exams> exams = new List<Exams>();
+
+            Console.WriteLine("Nece eded Exam elave etmek isteyirsiz?");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Exam N" + i + 1);
+
+                Console.Write("Ad: ");
+                string studentName = Console.ReadLine();
+                Console.Write("Point: ");
+                int point = int.Parse(Console.ReadLine());
+                Console.Write("Movzu: ");
+                string subject = Console.ReadLine();
+                Console.Write("Baslangic Tarixi (YYYY-MM-DD HH:MM): ");
+                DateTime startDate = DateTime.Parse(Console.ReadLine());
+                Console.Write("Bitme Tarixi (YYYY-MM-DD HH:MM): ");
+                DateTime endDate = DateTime.Parse(Console.ReadLine());
+
+                exams.Add(new Exams(studentName, point, subject, startDate, endDate));
+            }
+
+
+            Console.WriteLine("Pointi 50den cox olanlar: ");
+            foreach (var exam in exams)
+            {
+                if (exam.Point > 50)
+                {
+                    Console.WriteLine($"{exam.Student}, {exam.Subject}, ({exam.Point} point), " +
+                        $"{exam.StartDate}, {exam.EndDate} ");
+                }
+            }
+
+            Console.WriteLine("1 saatdan cox cekenler: ");
+            foreach (var exam in exams)
+            {
+                TimeSpan difference = exam.StartDate - exam.EndDate;
+                int totalDaysToHours = difference.Days * 24;
+
+                if (difference.Hours + totalDaysToHours < 0)
+                {
+                    Console.WriteLine($"{exam.Student}, {exam.Subject}, ({(exam.EndDate - exam.StartDate).TotalHours} saat cekib)");
+                }
+
+            }
+        }
+    }
+}
